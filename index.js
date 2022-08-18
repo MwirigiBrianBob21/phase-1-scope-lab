bea3deb781973a0ce237001938306ae8bf51ecaf
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-08-19 00:09:24 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+// var bestCustomer = "ken";
+const leastFavoriteCustomer = "her"
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    
+}
+
+function setBestCustomer() {
+   bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+     bestCustomer = 'maybe bob'
+    //  overWriteBestCustomer();
+
+}
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "kaka"
+}
